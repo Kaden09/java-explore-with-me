27377afd37a7b14ac1926bc553d5266740bd16f1
kaden <ketkette7@gmail.com>
2026-08-29 23:43:13 +0400

    fixed stats error handler

--- ewm-stats/server/src/main/java/ru/practicum/ewm/exceptions/ErrorHandler.java
+++ ewm-stats/server/src/main/java/ru/practicum/ewm/exceptions/ErrorHandler.java
@@ -4,6 +4,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.MissingServletRequestParameterException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -14,7 +15,10 @@ import java.time.LocalDateTime;
 @RestControllerAdvice
 public class ErrorHandler {
 
-    @ExceptionHandler({MethodArgumentNotValidException.class, DateTimeException.class})
+    @ExceptionHandler({
+            MethodArgumentNotValidException.class,
+            DateTimeException.class,
+            MissingServletRequestParameterException.class})
     public ResponseEntity<ErrorResponse> handleValidationException(final Exception e) {
         log.warn("Ошибка валидации: {}", e.getMessage());
 
